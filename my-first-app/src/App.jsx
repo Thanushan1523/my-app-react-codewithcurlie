2fcb63bf8b72ff203cd7f5a80c8bd4e0cec5d06b
Thanushan1523 <thanushan1523@gmail.com>
2024-09-17 07:47:40 -0400

    props & types

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -2,15 +2,20 @@ import Header from "./Header/Header"
 import Footer from "./Footer";
 import Body from "./Body";
 import Card from "./Card";
+import Student from "./Student";
 
 
 function App() {
   return(
+ 
     
-    
-      
+    <>
+    <Student name="fhf" age={25} isStudent={true} />
+    <Student  />
     <Header/>
-     
+    <Card/>
+    
+    </>
   );
 
 
