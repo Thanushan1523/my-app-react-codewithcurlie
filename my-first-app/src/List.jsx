93f6f1eb2ffa395f2198c114b3fd7645160a5c96
Thanushan1523 <thanushan1523@gmail.com>
2024-09-21 07:43:53 -0400

    render listwith key and value

--- my-first-app/src/List.jsx
+++ my-first-app/src/List.jsx
@@ -1,9 +1,10 @@
 function List(){
-    const veggies =[{name:"onion",qty:9},{name:"tomato",qty:9},{name:"carrot" ,qty:10}];
-    veggies.sort()
+    const veggies =[{id:1,name:"onion",qty:9},{id:2,name:"tomato",qty:9},{id:3,name:"carrot" ,qty:10}];
+    // veggies.sort((a,b) =>b.name.localeCompare(a.name)) //reverse sorting
+    veggies.sort((a,b) =>a.qty -b.qty) 
 
     // const listItems= veggies.map( veg => <li> {veg} </li>)
-    const listItems= veggies.map( veg => <li key={veg.name}> {veg.name} </li>)
+    const listItems= veggies.map( veg => <li key={veg.id}> {veg.name} : &nbsp; <b >{veg.qty}</b></li>)
     return(listItems)
 }
 
