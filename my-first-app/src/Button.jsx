35132d63f7fbc79ebb8f657b17a8d2e03092a653
Thanushan1523 <thanushan1523@gmail.com>
2024-09-28 18:13:46 -0400

    VEDIO-15 onclick-with consle.log if/else conditions

--- my-first-app/src/Button.jsx
+++ my-first-app/src/Button.jsx
@@ -1,9 +1,20 @@
 function Button(){
 
-    const handleclick =() => console.log("im clicked ");
-    const handleclick2 =(name) => console.log( `${name}clicked me `);
+    // const handleclick =() => console.log("im clicked ");
+    // const handleclick2 =(name) => console.log( `${name}clicked me `);
+
+    let count=0
+    const handleclick =(name)=> {
+        if (count < 3){
+            count++ ;
+            console.log(`${name} , clicked me ${count} times`)
+        }
+        else{
+            console.log(`${name} , please stop `)
+        }
+    }
     return(
-        <button onClick ={() =>handleclick2 ("thanu") }> hey thanushan</button>);
+        <button onClick ={() =>handleclick ("thanu") }> hey thanushan</button>);
 
 
 }
