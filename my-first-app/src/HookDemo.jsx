ff4ebc99deabd4d12ffd846c13676efcb3677bea
Thanushan1523 <thanushan1523@gmail.com>
2024-10-01 08:33:08 -0400

    VEDIO-18 usestate-2

--- my-first-app/src/HookDemo.jsx
+++ my-first-app/src/HookDemo.jsx
@@ -5,11 +5,14 @@ import React ,{useState} from 'react';
 function HookDemo(){
  const [name ,setName]=useState("guest");
  const [age ,setAge]=useState(0);
+ const [isBoolValue ,setIsBoolValue]=useState(false);
  const updateName =() =>{
     setName("thanushan");
  }
 
  const incrementAge = () =>{setAge (age + 1)}
+ const updateBoolValue = ()=> {setIsBoolValue (!isBoolValue)}
+
  return(
     <div>
         <p>
@@ -23,6 +26,13 @@ function HookDemo(){
         </p>
         <button onClick={incrementAge}>
             click here
+        </button>
+
+        <p>
+            here BOOL:{isBoolValue ? "yes" : "no" }
+        </p>
+        <button onClick={updateBoolValue}>
+            BOLLL
         </button>
     </div>
     
