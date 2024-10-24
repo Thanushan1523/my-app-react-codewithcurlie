bfb67ec4b339d6fe4c5909df29bdb5bb5325b456
Thanushan1523 <thanushan1523@gmail.com>
2024-10-24 08:07:01 -0400

    VEDIO-20 onChange

--- my-first-app/src/HandlerDemo.jsx
+++ my-first-app/src/HandlerDemo.jsx
@@ -5,15 +5,26 @@ import React ,{useState} from "react";
 
 function HandlerDemo(){
     const [name,setName] =useState(" ");
+    const [Qty ,setQty] =useState(1);
 
     function handleInput(event){
         setName(event.target.value)
     }
+
+    function handleNumber(event){
+        setQty(event.target.value)
+    }
+
+
     return (
         <div>
             <input value={name} onChange={handleInput}/>
             <p>Name:{name}</p>
+
+            <input value={Qty} onChange={handleNumber} type="number"/>
+            <p>Quantity:{Qty}</p>
         </div>
+
     )
 
 }
