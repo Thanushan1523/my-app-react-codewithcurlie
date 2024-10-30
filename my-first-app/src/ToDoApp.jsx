4209ed2d9abbe9751039a82fd71b022b24bb55e0
Thanushan1523 <thanushan1523@gmail.com>
2024-10-30 07:48:48 -0400

    VEDIO-23 ToDoApp

--- my-first-app/src/ToDoApp.jsx
+++ my-first-app/src/ToDoApp.jsx
@@ -1,10 +1,18 @@
  import { useState } from "react";
 import React  from "react";
+
+
  function ToDoApp (){
+    const[input ,setInput] =useState("") 
+    function handleInput(event){
+        setInput(event.target.value)
+    }
     return(
         <div>
             <hi> ToDo App</hi>
-            <input type="text" placeholder="enter the task"/>
+            <input type="text" value={input} onChange={handleInput}  placeholder="enter the task"/>
+            <br/>
+            <br/>
             <br/>
             <button> Add ToDo</button>
             <ul>
