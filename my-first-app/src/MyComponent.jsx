0e3c7b8b472ff4b76d195d12d2d1d6ef61a80e0e
Thanushan1523 <thanushan1523@gmail.com>
2024-11-02 20:48:44 -0400

    VEDIO-25 Updating the objects using the state

--- my-first-app/src/MyComponent.jsx
+++ my-first-app/src/MyComponent.jsx
@@ -1,8 +1,18 @@
 import React,{useState} from "react";
 
 function MyComponentt(){
+    const[cycle ,setCycle]=useState(
+        {type:"moutauin",
+            material:"carbon steel",
+            year:2024,
+            color:"black"
+        }
+        
+    )
     return(
-            <h1>fn</h1>
+            <div>
+                <p>my favaroite cycl is :{cycle.type}</p>
+            </div>
     )
     
 
