e858c607f5fa7c994ddee0d529219b887449dd54
Thanushan1523 <thanushan1523@gmail.com>
2024-11-02 21:29:56 -0400

    VEDIO-25 Updating the objects using the state

--- my-first-app/src/MyComponent.jsx
+++ my-first-app/src/MyComponent.jsx
@@ -9,9 +9,26 @@ function MyComponentt(){
         }
         
     )
+    function handleTypeChange(event){
+        setCycle(prevCycle =>({...prevCycle,type:event.target.value}))
+    }
+    function handleMaterialChange(event){
+        setCycle(prevCycle =>({...prevCycle,material:event.target.value}))
+    }
+    function handleColorChange(event){
+        setCycle(prevCycle =>({...prevCycle,color:event.target.value}))
+    }
+    function handleYearChange(event){
+        setCycle(prevCycle =>({...prevCycle,year:event.target.value}))
+    }
     return(
             <div>
-                <p>my favaroite cycl is :{cycle.type}</p>
+                <p>my favaroite cycl is :{cycle.type}{cycle.material}{cycle.color}{cycle.year}</p>
+                <input type="text" onChange={handleTypeChange} value={cycle.type}/> <br/>
+                <input type="text" onChange={handleMaterialChange} value={cycle.material}/> <br/>
+                <input type="text" onChange={handleColorChange} value={cycle.color}/> <br/>
+                <input type="number" onChange={handleYearChange} value={cycle.year}/>
+                <br/>
             </div>
     )
     
