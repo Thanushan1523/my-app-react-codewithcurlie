a3daef0e9e013e43ff9b40cc56b1b5a6a6557e33
Thanushan1523 <thanushan1523@gmail.com>
2024-11-07 07:32:23 -0500

    VEDIO-28 todo app with edit add delete options

--- my-first-app/src/EnhancedToDo.jsx
+++ my-first-app/src/EnhancedToDo.jsx
@@ -8,16 +8,68 @@ function EnhancedToDo(){
     const[editId ,setEditId]=useState(null);
     const[editText ,setEditText]=useState('');
 
+    const AddToDo =()=>{
+        if(inputText.trim()){
+            const newToDo ={
+                id:Date.now(),
+                text:inputText ,
+                completed :false
+            };
+            setToDos([...todos,newToDo])
+            setInputText ('');
+        }
+    }
+    const editTodo =(id ,text )=>{
+        setEditId(id);
+        setEditText(text);
+     }
+    const saveEdit =(id) => {
+        const updatedTodo =todos.map
+        ((todo)) => 
+            todo.id === id ? {...
+            todo ,text:editText} : todo)
+            setToDos (updatedTodo);
+            setEditId(null);
+            setEditText('');
+           
+        }
+    }
+
+
+    const deleteTodo =(id) =>{
+        const updatedTodos =todos.filter((todo) => todo.id !==id);
+        setToDos(updatedTodos)
+    }
+
     return(
     <div>
         <h1>ToDo List</h1>
-        <input type="text" value={inputText} onChange={(e)=> setInputText(e.target.value)} placeholder="Add your todo" />
+        <input
+        type="text" 
+        value={inputText} 
+        onChange={(e)=> setInputText(e.target.value)}
+        placeholder="Add your todo" 
+        />
         <ul>
-            {todos.map((todo)=> (<li key={todo.id}>{editId === todo.id ?(<input type="text"/>)}</li>}
+            {todos.map((todo)=> (
+                <li key={todo.id}>
+                    {editId === todo.id ?(
+                        <input 
+                        type="text"
+                        value={editText}
+                        onChange={(e)=> setEditText(e.target.value)}
+                    />
+                    ):(
+                        <span>{todo.text}</span>
+                    )
+                    )
+                        </li>
+                ))}
         </ul>
         
         
-        </div>)
+    </div>
+    );
 }
 
 export default EnhancedToDo;
