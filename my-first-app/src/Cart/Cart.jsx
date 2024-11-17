d39fa282b7d329b8ecf3d10279f24a4c208908bd
Thanushan1523 <thanushan1523@gmail.com>
2024-11-17 18:14:06 -0500

    VEDIO-31 add to cart

--- my-first-app/src/Cart/Cart.jsx
+++ my-first-app/src/Cart/Cart.jsx
@@ -1,5 +1,18 @@
+import React , { useState } from "react";
 
 function Cart(){
+    const [cart ,setCart]=useState([]);
+    const items =[
+       {id:1, name:'chicken briyani' , price:150 ,
+        description : 'aroma mixed chettinad briyani taste is good '
+       } ,
+       {id:2, name:'kottu' , price:200 ,
+        description : 'good '
+       } ,
+       {id:3, name:'kadalai' , price:250 ,
+        description : 'tatsty '
+       } 
+    ]
     return(<h1>hi</h1>)
 
 }
