51d61170176fda4b094fa7f22d4bc672c082ffdd
Thanushan1523 <thanushan1523@gmail.com>
2024-11-21 07:15:18 -0500

    VEDIO-33  add to cartstyling2

--- my-first-app/src/Cart/Cart.jsx
+++ my-first-app/src/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React , { useState } from "react";
+import './cart.css'
 
 function Cart(){
     const [cart ,setCart]=useState([]);
@@ -47,34 +48,41 @@ function Cart(){
     sum + item.quantity , 0);
     const totalPrice =cart.reduce((sum,item)=>sum + item.price*item.quantity , 0)
 
-    return(<div>
+    return(<div className="Cart">
         <h1>Items</h1>
-        <div>
-            {items.map((item)=>(<><h3>{item.name}</h3><p>{item.description}</p>
+        <div className="items-container ">
+            {items.map((item)=>(
+            <div key={item.id} className="item-box"> <h3>{item.name}</h3><p>{item.description}</p>
             <p> price: {item.price}</p>
-            <button onClick={()=> addToCart(item)}> Addtocart </button></>
+            <button onClick={()=> addToCart(item)}> Addtocart </button></div>
             ))}
 
             <div>
                 <h1>Cart</h1>
                 {cart.length === 0 && <p> NO items in the cart </p>}
                 {cart.length > 0 && (
-                    <div>
+                    <div className="cart-container">
                      <ul>
                         {cart.map((item) =>(  
                         <li key={item.id}>
                             <span>{item.name} - Quantity {item.quantity}</span>
+                            <div className="cart-button">
                             <button onClick= {()=> updateQuantity(item.id ,1)}> +</button>
                             <button onClick= {()=> updateQuantity(item.id ,-1)}> - </button>
                             <button onClick={()=> removeFromCart(item.id)}> Remove </button>
+                            </div>
                             </li>
                        ))}
                             </ul>   
-                    
-                 <p>Total items:<span> {totalItems}</span>
+                  <div className="cart-summary-box">
+                    <div className="cart-summary">
+                    <p>Total items:<span> {totalItems}</span>
                  </p>
                  <p>Total items:<span> {totalPrice}</span>
                  </p>
+                  </div>
+                  </div>  
+                 
                 </div>
                 )}
             </div>
