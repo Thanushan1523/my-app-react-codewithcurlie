f9dae0ab7ac74caf79de97adb734d6e2e50f5fa4
Thanushan1523 <thanushan1523@gmail.com>
2024-11-30 18:40:25 -0500

    VEDIO-35 USEEFFECT-WITH two  DEPENCIES

--- my-first-app/src/UseEffectDemo.jsx
+++ my-first-app/src/UseEffectDemo.jsx
@@ -1,18 +1,28 @@
 import React,{useState, useEffect} from "react";
 function UseEffectDemo(){
     const [count ,setCount] =useState(0);
+    const [color , setColor]= useState("blue");
     function addCount(){
           setCount(C => C+1)
+    }
+    function subCount(){
+        setCount(C => C-1)
+  }
+
+    function changeColor(){
+        setColor(c => c ==="blue" ? "red" :"blacks")
     }
 
 //  useEffect(() => {document.title=`count : ${count}`},[])
 //  useEffect(() => {document.title=`HI`} ,[])  
 
-useEffect(() => {document.title=`count : ${count}`},[count])
+useEffect(() => {document.title=`count : ${count} ${color}`},[count])
 
     return(<>
-    <p>count : {count}</p>
+    <p style={{color:color }}>count : {count}</p>
     <button onClick={addCount}>Add</button>
+    <button onClick={subCount}>Sub</button>
+    <button onClick={changeColor}>change color</button>
     </>)
  }
  
